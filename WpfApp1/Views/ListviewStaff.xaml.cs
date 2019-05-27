8d8c486b7ab00ffa9489d3127f8d08db2b3a2e4e
DerekKrook <45005652+DerekKrook@users.noreply.github.com>
2019-05-27 14:52:02 +0200

    derp

---
+++ WpfApp1/Views/ListviewStaff.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for ListViewStaff.xaml
+    /// </summary>
+    public partial class ListViewStaff : Window
+    {
+        public ListViewStaff()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSearchChild_Click(object sender, RoutedEventArgs e)
+        {
+            DbOperations db = new DbOperations();
+
+            children = db.GetChild(txtLastNameGuardian.Text);
+
+            listViewGuardian.ItemsSource = children;
+            listViewGuardian.DisplayMemberPath = "Fullinfo";
+        }
+    }
+}
