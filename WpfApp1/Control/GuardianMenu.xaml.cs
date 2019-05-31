6acbd513b8c2d4f0ad96c540d99419944b5b4e83
oscar nilsson <osni1998@hotmail.com>
2019-05-31 13:46:04 +0200

    profile

--- WpfApp1/Control/GuardianMenu.xaml.cs
+++ WpfApp1/Control/GuardianMenu.xaml.cs
@@ -57,7 +57,13 @@ namespace WpfApp1
 
         private void Profile_Click(object sender, RoutedEventArgs e)
         {
-            
+            var window = Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.IsActive);
+
+            Profile profile = new Profile();
+
+            profile.Show();
+
+            window.Close();
         }
 
         private void Attendance_Click(object sender, RoutedEventArgs e)
