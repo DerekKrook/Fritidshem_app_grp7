b1bd9270d07242093f99d267ecf5b66b7d57967e
marcus green <marcus.green92@hotmail.se>
2019-06-04 13:59:21 +0200

    Jättefin kod

--- WpfApp1/DbOperation/DbOperations.cs
+++ WpfApp1/DbOperation/DbOperations.cs
@@ -582,12 +582,7 @@ namespace WpfApp1
             using (IDbConnection connection = new NpgsqlConnection(ConnString.ConnVal("dbConn")))
             {
                 var output = connection.Query<Meal>($@"SELECT dates.day AS Day, dates.week AS Week, meals.name AS Name
-                    FROM ((((((((child
-                    INNER JOIN guardian_child on child.id=child_id)
-                    INNER JOIN guardian on guardian_id=guardian.id)
-                    INNER JOIN attendance on guardian.id=attendance.guardian_id)
-                    INNER JOIN category_attendance on category_attendance_id=category_attendance.id)
-                    INNER JOIN attendance_dates on attendance.id=attendance_id)
+                    FROM (((child                   
                     INNER JOIN meals on child.id=meals.child_id)
                     INNER JOIN meals_dates ON meals.id=meals_id)
                     INNER JOIN dates on meals_dates.dates_id=dates.id)
